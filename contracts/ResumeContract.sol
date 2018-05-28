d7afbaa7523c0c35797553ecc4512d57f02fd4ce
weera Kaew-uan <>
2018-05-29 00:55:54 +0700

    Reorder function

--- contracts/ResumeContract.sol
+++ contracts/ResumeContract.sol
@@ -60,19 +60,6 @@ contract ResumeContract is Owned {
 
 	mapping (address => ResumeModel) mResumeModels;
 
-	event ResumeEmptyEvent();
-
-	function triggerGetResumeByEvent(address accountId) public {
-		if(!mResumeModels[accountId].isNotEmpty){
-			triggerGetPersonalInfoByEvent(accountId);
-			triggerGetExperienceByEvent(accountId);
-			triggerGetEducationByEvent(accountId);
-			triggerGetSkillByEvent(accountId);
-		}else{
-			emit ResumeEmptyEvent();
-		}
-	}
-
 	event personalInfoEvent(bytes32 picUrl,
 													bytes32 name,
 													bytes32 phone,
@@ -82,21 +69,25 @@ contract ResumeContract is Owned {
 													bytes32 position,
 													string location);
 
-	function triggerGetPersonalInfoByEvent(address accountId) public {
+	event experienceEvent(bytes32 startDate,
+												bytes32 endDate,
+												bytes32 companyName,
+												bytes32 position,
+												string expText,
+ 												uint index);
 
-		if(!mResumeModels[accountId].isNotEmpty){
-			PersonalInfo memory info;
-			info = mResumeModels[accountId].personalInfo;
-			emit personalInfoEvent(	info.picUrl,
- 															info.name,
-															info.phone,
-															info.email,
- 															info.dateOfBirth,
- 															info.socialUrl,
-															info.currentPosition,
-															info.location );
-		}
-	}
+	event educationEvent(	bytes32 institute,
+												bytes32 degree,
+												bytes32 faculty,
+												bytes32 major,
+												bytes32 gpa,
+ 												uint index);
+
+	event skillEvent(	bytes32 skill,
+										bytes32 level,
+										uint index );
+
+	event ResumeEmptyEvent();
 
 
 	function addPersonalInfo(	bytes32 picUrl,
@@ -115,30 +106,6 @@ contract ResumeContract is Owned {
 		return true;
 	}
 
-	event experienceEvent(bytes32 startDate,
-												bytes32 endDate,
-												bytes32 companyName,
-												bytes32 position,
-												string expText,
- 												uint index);
-
-	function triggerGetExperienceByEvent(address accountId) public  {
-		if(mResumeModels[accountId].isNotEmpty){
-				Experience[] memory expList;
-				expList = mResumeModels[accountId].experiences;
-
-				for(uint i=0; i < expList.length; i++){
-					Experience memory exp = expList[i];
-					emit experienceEvent(	exp.startDate,
-																exp.endDate,
-																exp.companyName,
-																exp.position,
-																exp.expText,
-																i );
-				}
-			}
-	}
-
 	function addExperience(
 													bytes32 startDate,
 													bytes32 endDate,
@@ -148,6 +115,34 @@ contract ResumeContract is Owned {
 		Experience memory exp = Experience(	startDate, endDate, companyName,
  																				position, expText, true);
 		mResumeModels[msg.sender].experiences.push(exp);
+		return true;
+	}
+
+	function addEducation(
+													bytes32 institute,
+													bytes32 degree,
+													bytes32 faculty,
+													bytes32 major,
+													bytes32 gpa ) public returns(bool){
+		Education memory edu = Education(	institute, degree, faculty,
+ 																			major, gpa, true);
+		mResumeModels[msg.sender].educations.push(edu);
+		return true;
+	}
+
+	function addSkill(
+											bytes32 skill,
+											bytes32 level ) public returns(bool){
+		Skill memory skillStruct;
+		if(	level == SKILL_LEVEL_ADVANCED ||
+				level == SKILL_LEVEL_MEDIUM ||
+				level == SKILL_LEVEL_BASIC ||
+				level == SKILL_LEVEL_ADVANCED ){
+			skillStruct = Skill(skill, level, true);
+		}else{
+			skillStruct = Skill(skill, "", true);
+		}
+		mResumeModels[msg.sender].skills.push(skillStruct);
 		return true;
 	}
 
@@ -167,41 +162,6 @@ contract ResumeContract is Owned {
 		return false;
 	}
 
-	event educationEvent(	bytes32 institute,
-												bytes32 degree,
-												bytes32 faculty,
-												bytes32 major,
-												bytes32 gpa,
- 												uint index);
-
-	function triggerGetEducationByEvent(address accountId) public  {
-		if(mResumeModels[accountId].isNotEmpty){
-			Education[] memory eduList;
-			eduList = mResumeModels[accountId].educations;
-
-			for(uint i=0; i < eduList.length; i++){
-				Education memory edu = eduList[i];
-				emit educationEvent(edu.institute,
-														edu.degree,
-														edu.faculty,
-														edu.major,
-														edu.gpa,
-														i );
-			}
-		}
-	}
-
-	function addEducation(
-													bytes32 institute,
-													bytes32 degree,
-													bytes32 faculty,
-													bytes32 major,
-													bytes32 gpa ) public returns(bool){
-		Education memory edu = Education(	institute, degree, faculty,
- 																			major, gpa, true);
-		mResumeModels[msg.sender].educations.push(edu);
-		return true;
-	}
 
 	function updateEducation(
 														bytes32 institute,
@@ -217,38 +177,6 @@ contract ResumeContract is Owned {
 			return true;
 		}
 		return false;
-	}
-
-	event skillEvent(	bytes32 skill,
-										bytes32 level,
-										uint index );
-
-	function triggerGetSkillByEvent(address accountId) public  {
-		if(mResumeModels[accountId].isNotEmpty){
-			Skill[] memory skillList;
-			skillList = mResumeModels[accountId].skills;
-
-			for(uint i=0; i < skillList.length; i++){
-				Skill memory skill = skillList[i];
-				emit skillEvent(skill._skill, skill.level, i);
-			}
-		}
-	}
-
-	function addSkill(
-											bytes32 skill,
-											bytes32 level ) public returns(bool){
-		Skill memory skillStruct;
-		if(	level == SKILL_LEVEL_ADVANCED ||
-				level == SKILL_LEVEL_MEDIUM ||
-				level == SKILL_LEVEL_BASIC ||
-				level == SKILL_LEVEL_ADVANCED ){
-			skillStruct = Skill(skill, level, true);
-		}else{
-			skillStruct = Skill(skill, "", true);
-		}
-		mResumeModels[msg.sender].skills.push(skillStruct);
-		return true;
 	}
 
 	function updateSkill(
@@ -270,6 +198,81 @@ contract ResumeContract is Owned {
 			return true;
 		}
 		return false;
+	}
+
+	function triggerGetResumeByEvent(address accountId) public {
+		if(!mResumeModels[accountId].isNotEmpty){
+			triggerGetPersonalInfoByEvent(accountId);
+			triggerGetExperienceByEvent(accountId);
+			triggerGetEducationByEvent(accountId);
+			triggerGetSkillByEvent(accountId);
+		}else{
+			emit ResumeEmptyEvent();
+		}
+	}
+
+	function triggerGetPersonalInfoByEvent(address accountId) public {
+
+		if(!mResumeModels[accountId].isNotEmpty){
+			PersonalInfo memory info;
+			info = mResumeModels[accountId].personalInfo;
+			emit personalInfoEvent(	info.picUrl,
+ 															info.name,
+															info.phone,
+															info.email,
+ 															info.dateOfBirth,
+ 															info.socialUrl,
+															info.currentPosition,
+															info.location );
+		}
+	}
+
+
+
+	function triggerGetExperienceByEvent(address accountId) public  {
+		if(mResumeModels[accountId].isNotEmpty){
+				Experience[] memory expList;
+				expList = mResumeModels[accountId].experiences;
+
+				for(uint i=0; i < expList.length; i++){
+					Experience memory exp = expList[i];
+					emit experienceEvent(	exp.startDate,
+																exp.endDate,
+																exp.companyName,
+																exp.position,
+																exp.expText,
+																i );
+				}
+			}
+	}
+
+	function triggerGetEducationByEvent(address accountId) public  {
+		if(mResumeModels[accountId].isNotEmpty){
+			Education[] memory eduList;
+			eduList = mResumeModels[accountId].educations;
+
+			for(uint i=0; i < eduList.length; i++){
+				Education memory edu = eduList[i];
+				emit educationEvent(edu.institute,
+														edu.degree,
+														edu.faculty,
+														edu.major,
+														edu.gpa,
+														i );
+			}
+		}
+	}
+
+	function triggerGetSkillByEvent(address accountId) public  {
+		if(mResumeModels[accountId].isNotEmpty){
+			Skill[] memory skillList;
+			skillList = mResumeModels[accountId].skills;
+
+			for(uint i=0; i < skillList.length; i++){
+				Skill memory skill = skillList[i];
+				emit skillEvent(skill._skill, skill.level, i);
+			}
+		}
 	}
 
 }
